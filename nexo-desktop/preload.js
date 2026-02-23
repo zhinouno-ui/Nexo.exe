93023f8f3b78202391fb86b5767a9e6fef6db75d
zhinouno-ui <zhinouno@gmail.com>
2026-02-23 06:15:52 -0300

    Fix updater status handler block after 1.1.10 behavior rollback

---
+++ nexo-desktop/preload.js
@@ -0,0 +1,26 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('nexoStore', {
+  getAll: () => ipcRenderer.invoke('store:getAll'),
+  setAll: (data) => ipcRenderer.invoke('store:setAll', data),
+  patch: (partial) => ipcRenderer.invoke('store:patch', partial),
+  backupNow: () => ipcRenderer.invoke('store:backupNow'),
+  openDataFolder: () => ipcRenderer.invoke('app:openDataFolder'),
+  exportBackup: () => ipcRenderer.invoke('app:exportBackup'),
+  importContactsChunk: (payload) => ipcRenderer.invoke('store:importContactsChunk', payload)
+});
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  openExternal: (url) => ipcRenderer.invoke('external:open', url),
+  checkForUpdates: () => ipcRenderer.invoke('updater:check'),
+  installUpdate: () => ipcRenderer.invoke('updater:install'),
+  getAppVersion: () => ipcRenderer.invoke('app:getVersion'),
+  zoomIn: () => ipcRenderer.invoke('zoom:in'),
+  zoomOut: () => ipcRenderer.invoke('zoom:out'),
+  zoomReset: () => ipcRenderer.invoke('zoom:reset'),
+  onUpdaterStatus: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('updater:status', listener);
+    return () => ipcRenderer.removeListener('updater:status', listener);
+  }
+});
